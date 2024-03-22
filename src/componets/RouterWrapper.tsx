fbd6b581651285c125fc8bbae754ff946a5c11f8
Mus3b3bdo <musabashra1995@gmail.com>
2024-03-22 23:12:41 +0300

    add babel and basename

--- src/componets/RouterWrapper.tsx
+++ src/componets/RouterWrapper.tsx
@@ -4,16 +4,19 @@ import ErrorPage from "../Pages/ErrorPage";
 import DetailsPage from "../Pages/DetailsPage";
 
 export default function RouterWrapper() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/details/:countryCode",
-      element: <DetailsPage />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <HomePage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/details/:countryCode",
+        element: <DetailsPage />,
+      },
+    ],
+    { basename: "/world-api" }
+  );
   return <RouterProvider router={router} />;
 }
